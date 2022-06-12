1e97446709d17e810e50c0eef7dc5896138c749e
Sourabh Chauhan <sourabh.kumar35@gmail.com>
2022-06-13 03:10:02 +0530

    Commit

---
+++ src/assets/icon-ideas.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {   faChartBar } from '@fortawesome/fontawesome-free-solid'
+
+
+export default (props) => (
+    <FontAwesomeIcon icon={faChartBar} />
+  
+);
